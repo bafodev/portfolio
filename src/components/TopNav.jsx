fbe285722e333519ba8a0cb033003107e67d3864
Bafodé <bafoded.72@gmail.com>
2026-02-02 15:22:33 +0100

    Correction CV - MAJ Portfolio 2/02

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -20,7 +20,7 @@ export default function TopNav() {
 
   const handleClick = () => {
     window.open(
-      "./CV_TOURE_Bafode0102.pdf",
+      "./CV_TOURE_Bafode0202.pdf",
       "_blank",
       "noopener,noreferrer"
     );
